377079afc42f3896d32353a83a01239c94fc16eb
antoruizx <83556067+antoruizx@users.noreply.github.com>
2023-09-22 01:27:13 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import logoLinkedin from "../assets/img/linkedin.png";
 
 export default function Footer() {
     return (
-        <footer className="page-footer font-small pt-4 footer"> 
+        <footer className="page-footer font-small pt-4 footer">
             <div className="container-fluid text-center text-md-left pt-5 mt-5">
                 <div className="row justify-content-center">
                     <div className="col-md-6 mt-md-0 mt-3">
@@ -14,25 +14,28 @@ export default function Footer() {
                         <p>info@cryptoexchange.com</p>
                         <p>Crisostomo Alvarez 700, San Miguel de Tucumán.</p>
                     </div>
+                    <div className="col-md-3 mb-md-0 mb-3 footer-links">
+                        <a href="#!">
+                            <img src={logoFacebook}
+                                alt="facebook-logo"/>
+                        </a>
+                        <a href="#!">
+                            <img src={logoInstagram}
+                                alt="instagram-logo"/>
+                        </a>
+                        <a href="#!">
+                            <img src={logoTwitter}
+                                alt="twitter-logo"/>
+                        </a>
+                        <a href="#!">
+                            <img src={logoLinkedin}
+                                alt="linkedin-logo"/>
+                        </a>
+                    </div>
                 </div>
-
-            </div>
-            <div className="col-md-3 mb-md-0 mb-3 footer-links">
-                <a href="#!">
-                    <img src={logoFacebook} alt="facebook-logo" />
-                </a>
-                <a href="#!">
-                    <img src={logoInstagram} alt="instagram-logo" />
-                </a>
-                <a href="#!">
-                    <img src={logoTwitter} alt="twitter-logo" />
-                </a>
-                <a href="#!">
-                    <img src={logoLinkedin} alt="linkedin-logo" />
-                </a>
             </div>
             <div className="footer-copyright text-center py-3">
-            © 2023 Copyright
+                © 2023 Copyright
                 <a href="">cryptoexchange.com</a>
             </div>
         </footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Dripping Mood</a>
+                <a class="navbar-brand" href="#">Coin Page</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
