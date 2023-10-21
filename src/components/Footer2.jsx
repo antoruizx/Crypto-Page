60b7bd83f6e39cef836a213c0bedae3e9b6ebdd7
antoruizx <83556067+antoruizx@users.noreply.github.com>
2023-10-20 23:08:56 -0300

    footer changes

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -6,65 +6,66 @@ import linkedin from '../assets/img/linkedin.png';
 
 
 const Footer = () => {
-    return(
+    return (
         <div className="footer">
             <div className="sb__footer section__padding">
                 <div className="sb__footer-links">
                     <div className="sb__footer-links_div">
-                        <h4>For Business</h4>
-                        <a href="/employer">
-                            <p>Employer</p>
+                        <h4>Products</h4>
+                        <a href="/remeras">
+                            <p>Remeras</p>
                         </a>
-                        <a href="/healthplan">
-                            <p>Health Plan</p>
+                        <a href="/buzos">
+                            <p>Buzos</p>
                         </a>
-                        <a href="/induvidual">
-                            <p>Individual</p>
-                        </a>
-                    </div>
-                    <div className="sb__footer-links_div">
-                        <h4>Resources</h4>
-                        <a href="/resource">
-                            <p>Resource center</p>
-                        </a>
-                        <a href="/resource">
-                            <p>Testimonial</p>
-                        </a>
-                        <a href="/resource">
-                            <p>STV</p>
+                        <a href="/Accesorios">
+                            <p>Accesorios</p>
                         </a>
                     </div>
                     <div className="sb__footer-links_div">
-                        <h4>Partners</h4>
+                        <h4>About Us</h4>
+                        <a href="/quienessomos">
+                            <p>¿Quienes somos?r</p>
+                        </a>
+                        <a href="/ubicacion">
+                            <p>Ubicación</p>
+                        </a>
+                        <a href="/testimonios">
+                            <p>Testimonios</p>
+                        </a>
+                    </div>
+                    <div className="sb__footer-links_div">
+                        <h4>Contact</h4>
                         <a href="/employer">
-                            <p>Swing Tech</p>
+                            <p>RRSS</p>
                         </a>
                     </div>
 
                     <div className="sb_footer-links-div">
                         <h4>Coming soon</h4>
                         <div className="socialmedia">
-                            <p><img src={fb} alt="logofb" /></p>
-                            <p><img src={twitter} alt="logotw" /></p>
-                            <p><img src={linkedin} alt="logolk" /></p>
+                            <p><img src={fb}
+                                    alt="logofb"/></p>
+                            <p><img src={twitter}
+                                    alt="logotw"/></p>
+                            <p><img src={linkedin}
+                                    alt="logolk"/></p>
                         </div>
                     </div>
                 </div>
-            <hr />
 
-            <div className="sb__footer-bellow">
-                <div className="sb__footer-copyright">
-                    <p>
-                    ©{new Date().getFullYear()} Nadia Antonella Ruiz - Proyecto Final FullStack. All right reserved.
-                    </p>
+
+                <div className="sb__footer-bellow">
+                    <hr/>
+                    <div className="sb__footer-copyright">
+                        <p>
+                            ©{
+                            new Date().getFullYear()
+                        }
+                            Nadia Antonella Ruiz - Proyecto Final FullStack. All right reserved.
+                        </p>
+                    </div>
                 </div>
-                <div className="sb__footer-below_links">
-                    <a href="/terms"><div><p>Terms & Conditions</p></div></a>
-                    <a href="/privacy"><div><p>Privacy</p></div></a>
-                    <a href="/security"><div><p>Security</p></div></a>
-                    <a href="/cookie"><div><p>Cookie Declaration</p></div></a>
-                </div>
-            </div>
 
             </div>
         </div>
