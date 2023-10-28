eca2e7ae41bc54ccd889376e63895cb157fbac44
antoruizx <83556067+antoruizx@users.noreply.github.com>
2023-10-28 03:04:09 -0300

    signin changes

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -8,47 +8,29 @@ const URL = "mongodb:http://127.0.0.1:27017/userSystem"
 
 function SignInPage() {
 
-    const [formValues , setFormValues] = useState({
-        name: "anto",
-        username: "anto23",
-        password: "1234"
-    })
-
-    const handleChange = (event) => {
-        setFormValues({
-            ...formValues,
-            [event.target.name]: event.target.value
-        });
-    }
-
-    useEffect(() => {
-        axios.get(`${URL}/1`).then(response => setFormValues(response.data));
-    },[]);
-
-
       
     return (
         <>
         <Form>
             <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label name="name">Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter your name" value={formValues.name} onChange={handleChange}/>
+                <Form.Control type="name" placeholder="Enter your name"/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicUsername">
                 <Form.Label name="username">Username</Form.Label>
-                <Form.Control type="text" name="username" value={formValues.username} onChange={handleChange} placeholder="Enter your username"/>
+                <Form.Control type="text" name="username"  placeholder="Enter your username"/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label name="password">Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter your password" value={formValues.password} onChange={handleChange}/>
+                <Form.Control type="password" placeholder="Enter your password"/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Remember me"/>
             </Form.Group>
             <div>
-                <Button type="button" className="btn btn-primary">Signin</Button>
+            <a href="/signin" type="button" className="btn btn-success">Signin</a>
             </div>
         </Form>
         </>
